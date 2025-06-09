9e446e33467b541ec1aeb70b752df30ea6b41157
rimjhim005 <jahanrimjhim115@gmail.com>
2025-06-09 13:09:24 +0600

    updated

--- main.cpp
+++ main.cpp
@@ -21,6 +21,26 @@ int main()
         cout<<"Your SGPA is: ";
         cout<<Total_sgpa/semester_number;
     }
+     else if(choice == "SGPA" || choice == "sgpa")
+    {
+        float total_credit,total_course,sgpa,obtained_grade_point,credit,creditXgrade,total_creditXgrade=0;
+        cout<<"Enter the Total Credit"<<endl;
+        cin>>total_credit;
+        cout<<"Enter the number of total courses: "<<endl;
+        cin>>total_course;
+        for(int i=1;i<=total_course;i++)
+        {   cout<<"Enter the credit for course "<<i<<endl;
+            cin>>credit;
+            cout<<"Enter your obtained grade point: "<<i<<endl;
+            cin>>obtained_grade_point;
+            creditXgrade = credit * obtained_grade_point;
+            //cout<< creditXgrade;
+            total_creditXgrade += creditXgrade;
+            sgpa = total_creditXgrade / total_credit; 
+            
+        }
+        cout<<"Your SGPS is: "<<sgpa<<endl;
+    }
 
     return 0;
 }
